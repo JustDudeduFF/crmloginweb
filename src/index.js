497fed7bbbe3498a83f3523761ceb206737eba88
JustDudeduFF <authebaba.yt@gmail.com>
2025-11-26 19:26:05 +0530

    some changes

--- src/index.js
+++ src/index.js
@@ -35,14 +35,11 @@ root.render(
       </nav>
       <Routes>
         <Route path="/" element={<App />} />
-        {/* <Route path="/CustomerLogin" element={<CustomerLogin />} />
-        <Route path="/LandingPage" element={<LandingPage />} /> */}
+        <Route path="/CustomerLogin" element={<CustomerLogin />} />
+        <Route path="/LandingPage" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
